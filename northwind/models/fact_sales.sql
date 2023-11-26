cfec9671f44021e8a78555c407785274b7f46708
Sravan Kumar Mangalagiri <smangala@g.syr.edu>
2023-11-26 17:59:37 +0000

    commit and sync

--- northwind/models/fact_sales.sql
+++ northwind/models/fact_sales.sql
@@ -1,26 +1,32 @@
-WITH 
-stg_orders AS (
-    SELECT
-        OrderID,
-        {{ dbt_utils.generate_surrogate_key(['employeeid']) }} AS employeekey,
-        {{ dbt_utils.generate_surrogate_key(['customerid']) }} AS customerkey,
-        REPLACE(TO_DATE(orderdate)::VARCHAR, '-', '')::INT AS orderdatekey
-    FROM {{source('northwind','Orders')}}
-),
+with
+    stg_orders as (
+        select
+            orderid,
+            {{ dbt_utils.generate_surrogate_key(["employeeid"]) }} as employeekey,
+            {{ dbt_utils.generate_surrogate_key(["customerid"]) }} as customerkey,
+            replace(to_date(orderdate)::varchar, '-', '')::int as orderdatekey
+        from {{ source("northwind", "Orders") }}
+    ),
 
-stg_order_details AS (
-    SELECT
-        orderid,
-        productid,
-        {{ dbt_utils.generate_surrogate_key(['productid']) }} AS productkey,
-        SUM(Quantity) AS quantityonorder,
-        SUM(Quantity * UnitPrice) AS extendedpriceamount,
-        SUM(Quantity * UnitPrice * Discount) AS discountamount,
-        SUM(Quantity * UnitPrice * (1 - Discount)) AS soldamount
-    FROM {{source('northwind','Order_Details')}}
-    GROUP BY orderid, productid
-)
+    stg_order_details as (
+        select
+            orderid,
+            productid,
+            {{ dbt_utils.generate_surrogate_key(["productid"]) }} as productkey,
+            sum(quantity) as quantityonorder,
+            sum(quantity * unitprice) as extendedpriceamount,
+            sum(quantity * unitprice * discount) as discountamount,
+            sum(quantity * unitprice * (1 - discount)) as soldamount
+        from {{ source("northwind", "Order_Details") }}
+        group by orderid, productid
+    )
 
-SELECT o.*, od.productkey, od.quantityonorder, od.extendedpriceamount, od.discountamount, od.soldamount
-FROM stg_orders o 
-JOIN stg_order_details od ON o.orderid = od.orderid
+select
+    o.*,
+    od.productkey,
+    od.quantityonorder,
+    od.extendedpriceamount,
+    od.discountamount,
+    od.soldamount
+from stg_orders o
+join stg_order_details od on o.orderid = od.orderid
